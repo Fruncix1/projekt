7fd53682492880c027ce4ee8c7d4c788888ebfca
Fruncix1 <smieszektube@gmail.com>
2024-10-18 17:21:16 +0200

    MainWindow.xaml.cs

--- Lista zadań/Lista zadań/MainWindow.xaml.cs
+++ Lista zadań/Lista zadań/MainWindow.xaml.cs
@@ -15,9 +15,10 @@ using System.Windows.Shapes;
 
 namespace Lista_zadań
 {
-
     public partial class MainWindow : Window
     {
+        private TaskManager _taskManager = new TaskManager();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,16 +26,47 @@ namespace Lista_zadań
 
         private void Add_Task(object sender, EventArgs e)
         {
-            AddTaskToList(TaskInput.Text);
+            _taskManager.AddTask(TaskInput.Text);
+            TaskInput.Clear();
+            RefreshTaskList();
         }
 
-        private void AddTaskToList(string task)
+        private void Remove_Task(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(task))
+            if (TaskList.SelectedItem != null)
+            {
+                _taskManager.RemoveTask(TaskList.SelectedItem.ToString());
+                RefreshTaskList();
+            }
+        }
+
+        private void RefreshTaskList()
+        {
+            TaskList.Items.Clear();
+            foreach (var task in _taskManager.Tasks) 
             {
                 TaskList.Items.Add(task);
-                TaskInput.Clear();
             }
+        }
+
+        public class TaskManager
+        {
+            private List<string> _tasks = new List<string>();
+
+            public void AddTask(string task)
+            {
+                if (!string.IsNullOrEmpty(task))
+                {
+                    _tasks.Add(task);
+                }
+            }
+
+            public void RemoveTask(string task)
+            {
+                _tasks.Remove(task);
+            }
+
+            public IReadOnlyList<string> Tasks => _tasks.AsReadOnly();
         }
     }
 }
